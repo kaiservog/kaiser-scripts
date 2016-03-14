0320df39d0df3e5698a7f6d65c92ab85c8a69f3b
unknown <cvoginski@DT017704E.BRQ.COM>
2016-03-14 17:06:11 -0300

    Add whats.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kaiser-scripts
 Random scripts
+
+
+# Whats.sh
+	send a whatsapp message (needs to use WA Proxy) $1 = message $2 = Sao Paulo Number
---
+++ whats.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/bash
+
+CLIENT_ID="FREE_TRIAL_ACCOUNT"
+CLIENT_SECRET="PUBLIC_SECRET"
+
+read -r -d '' jsonPayload << _EOM_
+  {
+    "number": "5511$2",
+    "message": "$1"
+  }
+_EOM_
+
+curl -X POST \
+     -H "X-WM-CLIENT-ID: $CLIENT_ID" \
+     -H "X-WM-CLIENT-SECRET: $CLIENT_SECRET" \
+     -H "Content-Type: application/json" \
+     -d "$jsonPayload"   \
+     http://api.whatsmate.net/v1/whatsapp/queue/message
