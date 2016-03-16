5924bfc8f706e9d5e7ef60e0e7aabf67b7a3ab1d
Kaiservog <kaiservog@gmail.com>
2016-03-16 03:58:55 -0300

    one more commit

--- pirate-find.py
+++ pirate-find.py
@@ -1,10 +1,30 @@
+import sys
+import re
 from tpb import TPB
 from tpb import CATEGORIES, ORDERS
 
+seeking = sys.argv[1]
+minSize = sys.argv[2]
+scale = sys.argv[3]
+
+if scale == 'G': scale = "GiB"
+elif scale == 'M': scale = "MiB"
+else: exit(1)
+
+print('Seeking for ' + seeking)
+
 t = TPB('https://thepiratebay.org')
-result = t.search('$1').order(ORDERS.SEEDERS.ASC).page(1)
+result = t.search(seeking).order(ORDERS.SEEDERS.ASC).page(1)
 
+files = []
 for torrent in result: 
+	match = re.match(r'.*Size ([\d\.]*?)\s(\w\w\w).*', torrent.description.text)
+	if match:
+		files.append({'size': match.group(1), 'scale': match.group(2)})
 
-	print(torrent.description)
+
+
+files = filter(lambda e: e['scale'] == scale and e['size'] >= minSize, files)
+
+for file in files: print(file)
 
