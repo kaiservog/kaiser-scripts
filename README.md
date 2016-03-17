087fc0d3f5a8f17b3be8457cd807d30a888686b6
Kaiservog <kaiservog@gmail.com>
2016-03-16 23:31:31 -0300

    finished pirate-movie-find

--- README.md
+++ README.md
@@ -5,5 +5,14 @@ Random scripts
 # Whats.sh
 	send a whatsapp message (needs to use WA Proxy) $1 = message $2 = Sao Paulo Number
 
-# find-pirate.sj (dev)
-	Find some torrent on piratebay
+# pirate-movie-find.py
+	Find movie torrent on piratebay (TODO: filter movie by year)
+	! depends on https://github.com/kaiservog/TPB
+
+	$1 = movie title
+	$2 = minimum numer size
+	$3 = scale of the size
+	
+	python pirate-find.py deadpool 2 G
+
+
--- pirate-movie-find.py
+++ pirate-movie-find.py
@@ -11,20 +11,19 @@ if scale == 'G': scale = "GiB"
 elif scale == 'M': scale = "MiB"
 else: exit(1)
 
-print('Seeking for ' + seeking)
-
 t = TPB('https://thepiratebay.org')
-result = t.search(seeking).order(ORDERS.SEEDERS.ASC).page(1)
+result = t.search(seeking).order(ORDERS.SEEDERS.DES).page(1)
 
 files = []
 for torrent in result: 
 	match = re.match(r'.*Size ([\d\.]*?)\s(\w\w\w).*', torrent.description.text)
 	if match:
-		files.append({'size': match.group(1), 'scale': match.group(2)})
-
+		files.append({'size': match.group(1), 'scale': match.group(2), 'url':torrent.magnet_link, 'title':torrent.title})
 
 
 files = filter(lambda e: e['scale'] == scale and e['size'] >= minSize, files)
 
-for file in files: print(file)
+for file in files: 
+	print(file['title'], file['size']+file['scale'], file['url'])
+	print("")
 
